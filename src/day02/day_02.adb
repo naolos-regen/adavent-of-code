de86effca9dbf9d297834c3cde6b74056eed2bae
naolos-regen <naolos.regen@proton.me>
2025-06-25 22:48:57 +0200

    other try

--- src/day02/day_02.adb
+++ src/day02/day_02.adb
@@ -1,131 +1,51 @@
 with Ada.Text_IO; 		use Ada.Text_IO;
-with Ada.Strings.Fixed;		use Ada.Strings.Fixed;
-with Ada.Strings.Unbounded;	use Ada.Strings.Unbounded;
 
 package body Day_02 is
-	
-	type Line_Array is array (Positive range <>) of Ada.Strings.Unbounded.Unbounded_String;
-	type Line_Array_Access is access Line_Array;
 
-	type Cubes is (Red, Blue, Green);
-	type Cube_Counts is array (Cubes) of Integer;
-	
-	type Draw is array (Positive range <>) of Cube_Counts;
-	type Draw_Access is access Draw;
+	type Cubes 	is (Red, Blue, Green);
+	type Red_Cube	is new Integer range 0 .. 12;
+	type Blue_Cube	is new Integer range 0 .. 14;
+	type Green_Cube	is new Integer range 0 .. 13;
 
-	type Game is record
-		Game_Index	: Integer;
-		Draws		: Draw_Access;
+	type Numbered_Cube (Color : Cubes) is record
+		case Color is
+			when Red	=> Red_Count   : Red_Cube;
+			when Blue	=> Blue_Count  : Blue_Cube;
+			when Green	=> Green_Count : Green_Cube;
+		end case;
 	end record;
-	type Game_Array is array (Positive range <>) of Game;
-	type Game_Access is access Game_Array;
-	
-	function Parse_Color(S : String) return Cubes is
-		No_Such_Color_Exception : exception;
+
+	procedure Set_Cube_Count(Cube : in out Numbered_Cube; New_Count : Integer) is
 	begin
-		if S = "red" then
-			return Red;
-		elsif S = "blue" then
-			return Blue;
-		elsif S = "green" then
-			return Green;
-		else
-			raise No_Such_Color_Exception;
-		end if;
-	end Parse_Color;
+		case Cube.Color is 
+			when Red 	=> Cube.Red_Count   := Red_Cube  (New_Count);
+			when Blue 	=> Cube.Blue_Count  := Blue_Cube (New_Count);
+			when Green	=> Cube.Green_Count := Green_Cube(New_Count);
+		end case;
+	end Set_Cube_Count;
 
-	function Parse_Cube_Count(S : String) return Cube_Counts is
-		Count	: Cube_Counts := (others => 0);
-		Parts	: constant Line_Array := Line_Array'(1 => To_Unbounded_String(S));
+	-- Validate if Game is Valid Game
+	function Parse_Line(Game: String) return Boolean is
 	begin
-		for I in Parts'Range loop
-			declare
-				E	: constant String := Trim(To_String(Parts(I)), Side => Ada.Strings.Both);
-				N	: Integer;
-				C	: Cubes;
-				Space	: constant Natural := Index(E, " ");
-			begin
-				N := Integer'Value(E(1 .. Space - 1));
-				C := Parse_Color(Trim(E(Space + 1 .. E'Last), Side => Ada.Strings.Both));
-				Count(C) := N;
-			end;
-		end loop;
-		return  Count;
-	end Parse_Cube_Count;
-
-	function Split(S: String; Sep : Character) return Line_Array is
-		Parts_Count	: Natural 	:= 1;
-	begin
-		for I in S'Range loop
-			if S(I) = Sep then
-				Parts_Count 	:= Parts_Count + 1;
-			end if;
-		end loop;
-
-		declare
-			Result 	: Line_Array	(1 .. Parts_Count);
-			Index	: Natural	:= 1;
-			Start	: Positive	:= S'First;
-		begin
-			for I in S'Range loop
-				if S(I) = Sep then
-					Result(Index) 	:= To_Unbounded_String(S(Start .. I - 1));
-					Index 		:= Index + 1;
-					Start 		:= I + 1;
-				end if;
-			end loop;
-
-			Result(Index) := To_Unbounded_String(S(Start .. S'Last));
-			return Result;
-		end;
-
-	end Split;
+		return False;
+	end Parse_Line;
 
 	
 	procedure Solve(File_Path: String) is
-	   Max_Games     : constant Integer := 100;
-	   Parsed_Games  : Game_Access := new Game_Array(1 .. Max_Games);
-	   Game_Count    : Integer := 0;
-	   File          : File_Type;
-	   Line          : String (1 .. 200);
-	   Len           : Natural;
+		File	: File_Type;
+		CX	: Integer := 0;
 	begin
-		Put_Line("Reading file: " & File_Path);
 		Open(File => File, Mode => In_File, Name => File_Path);
-		while not End_Of_File(File) loop
-      			Get_Line(File, Line, Len);
-      			declare
-				L          : constant String := Line(1 .. Len);
-				Colon      : constant Natural := Index(L, ":");
-				Header     : constant String := L(1 .. Colon - 1);
-				B	   : constant String := L(Colon + 1 .. L'Last);
-				Draw_Parts : Line_Array := Split(B, ';');
-				Game_Id    : constant Integer := Integer'Value(L(6 .. Colon - 1)); -- "Game N"
-				Draws      : Draw_Access := new Draw(1 .. Draw_Parts'Length);
-      			begin
-				for I in Draw_Parts'Range loop
-					declare
-						Sub_Draws : Line_Array := Split(To_String(Draw_Parts(I)), ',');
-						Sum       : Cube_Counts := (others => 0);
-					begin
-						for J in Sub_Draws'Range loop
-							declare
-								E     : constant String := Trim(To_String(Sub_Draws(J)), Side => Ada.Strings.Both);
-								Space : constant Natural := Index(E, " ");
-								N     : constant Integer := Integer'Value(E(1 .. Space - 1));
-								C     : constant Cubes   := Parse_Color(Trim(E(Space + 1 .. E'Last), Side => Ada.Strings.Both));
-							begin
-								Sum(C) := N;
-							end;
-						end loop;
-						Draws(I) := Sum;
-					end;
-				end loop;
-				Game_Count := Game_Count + 1;
-				Parsed_Games(Game_Count) := (Game_Index => Game_Id, Draws => Draws);
+		while not End_Of_File (File) loop
+			declare
+				Line	: constant  String := Get_Line(File);
+			begin
+				Put_Line(Line);
+				if Parse_Line(Line) = True then 
+					CX := CX + 1;
+				end if;
 			end;
 		end loop;
 		Close(File);
-		Put_Line("Part 1 Day 2:" & Integer'Image(Game_Count));
 	end Solve;
 end Day_02;
