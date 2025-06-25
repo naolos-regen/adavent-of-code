713d408e1d3274cf506f730cf12df34e88786c29
naolos-regen <naolos.regen@proton.me>
2025-06-25 23:00:34 +0200

    I like these types, but I gotta understand  them first

--- src/day02/day_02.adb
+++ src/day02/day_02.adb
@@ -3,30 +3,45 @@ with Ada.Text_IO; 		use Ada.Text_IO;
 package body Day_02 is
 
 	type Cubes 	is (Red, Blue, Green);
-	type Red_Cube	is new Integer range 0 .. 12;
-	type Blue_Cube	is new Integer range 0 .. 14;
-	type Green_Cube	is new Integer range 0 .. 13;
 
-	type Numbered_Cube (Color : Cubes) is record
-		case Color is
-			when Red	=> Red_Count   : Red_Cube;
-			when Blue	=> Blue_Count  : Blue_Cube;
-			when Green	=> Green_Count : Green_Cube;
-		end case;
+	subtype Red_Cube_Count		is Integer range 0 .. 12;
+	subtype Blue_Cube_Count		is Integer range 0 .. 14;
+	subtype Green_Cube_Count	is Integer range 0 .. 13;
+
+	type Red_Cube is record
+		Cube : Cubes := Red;
+		Count: Red_Cube_Count;
 	end record;
 
-	procedure Set_Cube_Count(Cube : in out Numbered_Cube; New_Count : Integer) is
+	type Green_Cube is record
+		Cube : Cubes := Green;
+		Count: Green_Cube_Count;
+	end record;
+
+	type Blue_Cube is record
+		Cube : Cubes := Blue;
+		Count: Blue_Cube_Count;
+	end record;
+
+	type Cube_Set	is record
+		Red	: Red_Cube;
+		Blue	: Blue_Cube;
+		Green	: Green_Cube;
+	end record;
+
+	function Cube_Image(C: Cubes) return String is
 	begin
-		case Cube.Color is 
-			when Red 	=> Cube.Red_Count   := Red_Cube  (New_Count);
-			when Blue 	=> Cube.Blue_Count  := Blue_Cube (New_Count);
-			when Green	=> Cube.Green_Count := Green_Cube(New_Count);
+		case C is 
+			when Red   => return "red"  ;
+			when Blue  => return "blue" ;
+			when Green => return "green";
 		end case;
-	end Set_Cube_Count;
+	end Cube_Image;
 
 	-- Validate if Game is Valid Game
 	function Parse_Line(Game: String) return Boolean is
 	begin
+		Put_Line(Game);
 		return False;
 	end Parse_Line;
 
@@ -38,10 +53,10 @@ package body Day_02 is
 		Open(File => File, Mode => In_File, Name => File_Path);
 		while not End_Of_File (File) loop
 			declare
-				Line	: constant  String := Get_Line(File);
+				Game : constant  String := Get_Line(File);
 			begin
-				Put_Line(Line);
-				if Parse_Line(Line) = True then 
+				Put_Line(Game);
+				if Parse_Line(Game) = True then 
 					CX := CX + 1;
 				end if;
 			end;
