c9f6f4244306d973dcdb542ca776950dba99c280
naolos-regen <naolos.regen@proton.me>
2025-07-04 17:18:19 +0200

    Empty Array filled with '.' + Sol - Is_Number() -> 'n' | Is_Not_Dot() -> 'a'

--- src/day03/day_03.adb
+++ src/day03/day_03.adb
@@ -4,31 +4,42 @@ package body Day_03 is
 
 	subtype Index is Integer range 1 .. 140;
 	type Array_Of_Chars is array (Index, Index) of Character;
-	
+	Empty_Array : constant Array_Of_Chars := (others => (others => '.'));
 
-
-	procedure Lookout(Arr: Array_Of_Chars) is 
+	procedure Lookout(Arr: Array_Of_Chars; Sol: Array_Of_Chars) is 
 	begin
 		for I in Arr'Range (1) loop
 			for J in Arr'Range (2) loop
-				Put(Arr(J,I)'Image);
+				if Sol(J, I) = 'n' then
+					if J + 1 in Index and I + 1 in Index then
+						null;
+					end if;
+					if J - 1 in Index and I - 1 in Index then
+						null;
+					end if;
+				end if;
 			end loop;
-			New_Line;
 		end loop;
 	end Lookout;
 
 	-- Lookout for adjacent to a symbol is every possibility of +1 0 -1 arranged in 2 options
-	procedure Append(Arr: in out Array_Of_Chars; Line : String; I: Integer) is
-		Idx : Index := Index(I);
+	procedure Append(Arr, Sol: in out Array_Of_Chars; Line : String; I: Integer) is
+		Idx : constant Index 	:= Index(I);
 	begin
 		for J in Arr'Range (1) loop
 			Arr(J, Idx) := Line(J);
+			if Line(J) in '0' .. '9' then
+				Sol(J, Idx) := 'n';
+			elsif Line(J) /= '.' then
+				Sol(J, Idx) := 'a';
+			end if;
 		end loop;
 	end Append;
 
 	procedure Solve(File_Path: String) is
 		File	: File_Type;
-		Arr	: Array_Of_Chars;
+		Arr	: Array_Of_Chars := Empty_Array;
+		Sol	: Array_Of_Chars := Empty_Array;
 		Index : Natural := 1;
 	begin
 		Open (File => File, Mode => In_File, Name => File_Path);
@@ -36,11 +47,11 @@ package body Day_03 is
 			declare
 				Engine	: constant String := Get_Line(File);
 			begin
-				Append(Arr, Engine, Index);
+				Append(Arr, Sol, Engine, Index);
 				Index := Index + 1;
 			end;
 		end loop;
-		Lookout(Arr);
+		Lookout(Arr, Sol);
 
 		Close (File);
 	end Solve;
